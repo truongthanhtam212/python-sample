8663719afdb6df3819fac60769a1856cefb0efe2
truongthanhtam212 <83177704+truongthanhtam212@users.noreply.github.com>
2022-01-11 20:27:39 +0200

    Delete digifilter.py

--- digifilter.py
+++
@@ -1,7 +0,0 @@
-"""
-The function must remove all values from the list that are higher than the threshold.
-"""
-def filter_errors(a, b):
-    for x in a[:]:
-        if x > b:
-            a.remove(x)
